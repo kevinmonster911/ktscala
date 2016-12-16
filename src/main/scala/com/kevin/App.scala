6ef8bd1f5fb523dbf65a3ebf9681b99a5d20fb46
kevin <kevinmonster911@gmail.com>
2016-12-16 10:04:49 +0800

    for expression

--- src/main/scala/com/kevin/App.scala
+++ src/main/scala/com/kevin/App.scala
@@ -5,11 +5,33 @@ package com.kevin
  */
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  def foo(x : Array[String]) = x.foldRight("2")((a,b) => a + b)
   
   def main(args : Array[String]) {
     println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    println("concat arguments = " + foo(Array("3", "——5")))
+
+    val dogBreeds = List("Doberman", "Yorkshire Terrier")
+    for {breed <- dogBreeds if breed.contains("man")} println(breed)
+
+    for {
+      breed <- dogBreeds
+      if breed.contains("man")
+    } yield breed
+
+    val dogOptions = List(Some("a"), None, Some("b"), None)
+    for {
+      breedOption <- dogOptions if breedOption != None
+      breed <- breedOption
+      upcaseBread <- breed.toUpperCase
+    } println(upcaseBread)
+
+    for {
+      Some(breed) <- dogOptions
+      upcaseBread <- breed.toUpperCase
+    } println(upcaseBread)
+
+
   }
 
 }
